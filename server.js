2c8e1e4e4643d40c5e7d9088e84ea10db769e4c2
unknown <mcsalazar122@gmail.com>
2022-07-14 22:45:50 -0500

    add files

---
+++ server.js
@@ -0,0 +1,107 @@
+const fs = require('fs');
+const path = require('path');
+
+const express = require('express');
+const { notes } = require('./Develop/db/notes.json');
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+//parse incoming string or array data
+app.use(express.urlencoded({ extended: true }));
+
+
+app.use(express.json());
+//parse incoming JSON data
+
+app.use(express.static('public'));
+
+function filterByQuery(query, notesArray) {
+
+    let titleArray = [];
+    let filteredResults = notesArray;
+    if (query.title) {
+        if (typeof query.title === 'string') {
+            titleArray = [query.title];
+        } else {
+            titleArray = query.title;
+        }
+        titleArray.forEach(noteTitle => {
+            filteredResults = filteredResults.filter(
+                note => note.title.indexOf(noteTitle) !== -1
+            );
+        });
+    }
+
+    return filteredResults;
+}
+
+function findById(id, notesArray) {
+    const result = notesArray.filter(note => note.id === id)[0];
+    return result;
+}
+
+function createNewNote(body, notesArray) {
+    const note = body;
+    notesArray.push(note);
+    fs.writeFileSync(
+        path.join(__dirname, './Develop/db/notes.json'),
+        JSON.stringify({ notes: notesArray }, null, 2)
+    );
+    return note;
+}
+
+function validateNote(note) {
+    if (!note.title || typeof note.title !== 'string') {
+        return false;
+    }
+    if (!note.text || typeof note.text !== 'string') {
+        return false;
+    }
+    return true;
+}
+
+
+app.get('/api/notes', (req, res) => {
+    let results = notes;
+    if (req.query) {
+        results = filterByQuery(req.query, results);
+    }
+    res.json(results);
+});
+
+app.get('/api/notes/:id', (req, res) => {
+    const result = findById(req.params.id, notes);
+    if (result) {
+        res.json(result);
+    } else {
+        res.send(404);
+    }
+});
+
+app.post('/api/notes', (req, res) => {
+    //req body is incoming content length sets id to add 1 number to existing
+    req.body.id = notes.length.toString();
+    //add note to json file and notesArray
+    if (!validateNote(req.body)) {
+        res.status(400).send('The note must be text only');
+    } else {
+        const note = createNewNote(req.body, notes);
+        res.json(note);
+    }
+});
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/notes.html'));
+});
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}!`);
+});
